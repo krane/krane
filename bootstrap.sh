1611ad2dc4d273ef3dec2f129b711bb4919e7d08
David Castaneda <davidcastaneda@Davids-MBP.hsd1.fl.comcast.net>
2020-05-05 23:52:17 -0400

    Starts work on bootsrap.sh file

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,17 @@
-# Verify Requirements
-# Set ENV
-# Configure host 'on-start processes' file
-# Start Server
+#!/bin/bash
 
+# Verify docker is installed
+if [ ! -x "$(command -v docker)" ]; then
+    echo "Docker required to bootstrap krane-server"
+    exit 0
+fi
+
+
+# Set default env
+KRANE_SERVER_PATH=""
+
+# Configure host 'on-start processes' file
+
+# Start Server
+$KRANE_SERVER_PATH/server
 
