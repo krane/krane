65a1bd1e05ab05e31bb7ba168debe3175dbb5f16
David Castaneda <davidcastaneda@Davids-MBP.hsd1.fl.comcast.net>
2020-05-06 20:04:18 -0400

    Adds build script

---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+app=krane-server
+owner=biensupernice
+server_path="$PWD"/cmd/server
+bin_path=/usr/local/bin # Need to check if this should be overridable..
+
+echo "> Building $app"
+
+# Verify go is installed
+if [ ! -x "$(command -v go)" ]; then
+    echo "go required to package $app"
+    exit 0
+fi
+
+# Run the build
+cd "$server_path"
+
+go build -o "$bin_path"/$app -tags $owner
+echo "> $app installed succesfully"
+echo $'\nRun:'
+echo "> $app"
