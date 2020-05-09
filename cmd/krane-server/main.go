10db0cfc90f9e65f814db4bda09dbc7a4ce37a59
David Castaneda <dcast188@fiu.edu>
2020-05-09 13:37:57 -0400

    Add server config for port and log level

--- cmd/krane-server/main.go
+++ cmd/krane-server/main.go
@@ -3,15 +3,38 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 	"os/user"
 
 	"github.com/biensupernice/krane/server"
 	"github.com/dgraph-io/badger"
 )
 
+// Env
+var Port = os.Getenv("PORT")
+var LogLevel = os.Getenv("LOG_LEVEL")
+
 func main() {
 	db := startDB()
-	server.Run(db)
+
+	// Configure default env
+	cnfEnv()
+
+	cnf := &server.Config{Port: fmt.Sprintf(":%s", Port), LogLevel: LogLevel}
+	server.Run(*cnf, db)
+}
+
+// Configure default env
+func cnfEnv() {
+	// Set default port
+	if Port == "" {
+		Port = "8080"
+	}
+
+	// Set default loglevel
+	if LogLevel == "" {
+		LogLevel = "debug"
+	}
 }
 
 func startDB() *badger.DB {
