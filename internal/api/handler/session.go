8421f22dc7dc182832943b322eba76c109705ba5
David Castaneda <dcast188@fiu.edu>
2020-05-21 20:12:21 -0400

    Updated README

--- internal/api/handler/session.go
+++ internal/api/handler/session.go
@@ -11,10 +11,6 @@ import (
 
 // GetSessions : get current sessions
 func GetSessions(c *gin.Context) {
-	mySession, _ := c.Get("session")
-	log.Printf("My session: %v", mySession)
-
-	// Get sessions
 	sessionData := data.GetAll(data.SessionsBucket)
 
 	var sessions []Session
@@ -28,5 +24,5 @@ func GetSessions(c *gin.Context) {
 		sessions = append(sessions, s)
 	}
 
-	http.Created(c, sessions)
+	http.Ok(c, sessions)
 }
