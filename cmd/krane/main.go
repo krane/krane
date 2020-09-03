5156110d9df0610728c2b9fcfa928093c674bc20
David Castaneda <dcast188@fiu.edu>
2020-09-02 23:18:16 -0400

    Add initial scheduler func

--- cmd/krane/main.go
+++ cmd/krane/main.go
@@ -3,37 +3,95 @@ package main
 import (
 	"log"
 	"os"
+	"os/signal"
+	"strconv"
+	"syscall"
 
+	"github.com/docker/docker/client"
 	"github.com/sirupsen/logrus"
 
 	"github.com/biensupernice/krane/api"
+	job "github.com/biensupernice/krane/internal/jobs"
 	"github.com/biensupernice/krane/internal/logging"
+	"github.com/biensupernice/krane/internal/scheduler"
 	"github.com/biensupernice/krane/internal/storage"
 	"github.com/biensupernice/krane/internal/storage/boltdb"
-	"github.com/biensupernice/krane/pkg/bbq"
 	"github.com/biensupernice/krane/pkg/docker"
 )
 
 func init() {
 	log.Println("Starting Krane...")
 
-	logLvl := os.Getenv("LOG_LEVEL")
-	if logLvl == "" {
-		_ = os.Setenv("LOG_LEVEL", logging.INFO)
-	}
+	requireEnv("KRANE_PRIVATE_KEY")
+	envOrDefault("LOG_LEVEL", logging.INFO)
+	envOrDefault("LISTEN_ADDRESS", "127.0.0.1:8500")
+	envOrDefault("STORE_PATH", "/tmp/krane.db")
+	envOrDefault("WORKERPOOL_SIZE", "1")
+	envOrDefault("JOBQUEUE_SIZE", "1")
+
 	logging.ConfigureLogrus()
-
-	listenAddr := os.Getenv("LISTEN_ADDRESS")
-	if listenAddr == "" {
-		logrus.Fatal("LISTEN_ADDRESS must be set and non-empty")
-	}
-
 	docker.Init()
-	boltdb.Init()
-	bbq.InitJobQueue()
+	boltdb.Init(os.Getenv("STORE_PATH"))
 }
 
 func main() {
-	api.Run()
-	defer storage.Close()
+	// Docker Client
+	dockerClient, err := client.NewEnvClient()
+	if err != nil {
+		logrus.Fatalf("Unable to connect to Docker client %s", err.Error())
+	}
+
+	// Storage
+	store := storage.GetInstance()
+	defer store.Shutdown()
+
+	// Job Queue
+	jobQueueSize, _ := strconv.ParseUint(os.Getenv("JOBQUEUE_SIZE"), 10, 8)
+	jobQueue := job.NewJobQueue(uint(jobQueueSize))
+
+	// Job Enqueuer
+	jobEnqueuer := job.NewEnqueuer(&store, jobQueue)
+
+	// Job Worker Pool
+	wpSize, _ := strconv.ParseUint(os.Getenv("WORKERPOOL_SIZE"), 10, 8)
+	jobWorkers := job.NewWorkerPool(uint(wpSize), jobQueue, &store)
+	jobWorkers.Start()
+
+	// Scheduler
+	jobScheduler := scheduler.New(store, dockerClient, jobEnqueuer)
+	jobScheduler.Run()
+
+	go api.Run()
+
+	wait()
+
+	logrus.Info("Shutdown signal received")
+	jobWorkers.Stop()
+	logrus.Info("All workers done shutting down")
+}
+
+// wait : for a signal to quit
+func wait() {
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt, syscall.SIGTERM)
+	<-signalChan // Blocks here until interrupted
+}
+
+func requireEnv(key string) {
+	_, hasEnv := os.LookupEnv(key)
+	if !hasEnv {
+		log.Fatalf("Missing required env %s", key)
+	}
+}
+
+func envOrDefault(key string, fallback string) string {
+	val, hasEnv := os.LookupEnv(key)
+	if !hasEnv {
+		log.Printf("%s not set, defaulting to %s", key, fallback)
+		os.Setenv(key, fallback)
+		return fallback
+	}
+
+	logrus.Infof("%s already set with value %s", key, val)
+	return os.Getenv(key)
 }
--- internal/scheduler/scheduler.go
+++ internal/scheduler/scheduler.go
@@ -1,1 +1,42 @@
 package scheduler
+
+import (
+	"github.com/docker/docker/client"
+	"github.com/sirupsen/logrus"
+
+	job "github.com/biensupernice/krane/internal/jobs"
+	"github.com/biensupernice/krane/internal/storage"
+	"github.com/biensupernice/krane/internal/utils"
+)
+
+type Scheduler struct {
+	Store        storage.Storage
+	dockerClient *client.Client
+	Enqueuer     job.Enqueuer
+}
+
+func New(store storage.Storage, dockerClient *client.Client, jobEnqueuer job.Enqueuer) Scheduler {
+	return Scheduler{store, dockerClient, jobEnqueuer}
+}
+
+// 1. Fetch current state
+// 2. Fetch desired state
+// 3. Create required jobs
+func (sc *Scheduler) Run() {
+	logrus.Infof("Starting Scheduler")
+	sc.Enqueuer.WithHandler("DEPLOY_JOB", handleRunDeployment)
+	sc.Enqueuer.WithHandler("DELETE_JOB", handleDeleteDeployment)
+	sc.Enqueuer.Enqueue("DEPLOY_JOB", job.Args{"id": utils.MakeIdentifier()})
+	sc.Enqueuer.Enqueue("DELETE_JOB", job.Args{"id": utils.MakeIdentifier()})
+	return
+}
+
+func handleRunDeployment(args job.Args) error {
+	logrus.Infof("Running deployment job %v", args)
+	return nil
+}
+
+func handleDeleteDeployment(args job.Args) error {
+	logrus.Infof("Running deletion job %v", args)
+	return nil
+}
