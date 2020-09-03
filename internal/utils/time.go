fa26014df6610c92f71a6a004984e7fbee6b957d
David Castaneda <dcast188@fiu.edu>
2020-09-02 23:15:52 -0400

    func for creating identifiers

--- internal/utils/time.go
+++ internal/utils/time.go
@@ -1,9 +1,23 @@
 package utils
 
-import "time"
+import (
+	"crypto/rand"
+	"fmt"
+	"io"
+	"time"
+)
 
 // UTCDateString : Get the current date time in RFC3339 format
 func UTCDateString() string {
 	t := time.Now().Local()
 	return t.Format(time.RFC3339)
+}
+
+func MakeIdentifier() string {
+	b := make([]byte, 12)
+	_, err := io.ReadFull(rand.Reader, b)
+	if err != nil {
+		return ""
+	}
+	return fmt.Sprintf("%x", b)
 }
