c7cfd6bd6b88cc55b4f447e3f0ece898d21b17d7
David Castaneda <dcast188@fiu.edu>
2021-01-10 16:45:28 -0500

    (feat) update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,10 +5,9 @@ ensure_env() {
   local prompt_msg=$2
 
   if [ ! -z "$env" ]; then
-    read -p "$prompt_msg `echo $'\n> '`" value
+    read -p "$prompt_msg: `echo $'\n$ '`" value
     export "$env=$value"
   fi
-  printf "\n"
 }
 
 ensure_secure_env() {
@@ -16,19 +15,18 @@ ensure_secure_env() {
   local prompt_msg=$2
 
   if [ ! -z "$env" ]; then
-    read -s -p "$prompt_msg `echo $'\n> '`" value
+    read -s -p "$prompt_msg: `echo $'\n$ '`" value
     export "$env=$value"
-    printf "\n"
   fi
-  printf "\n"
+  echo ""
 }
 
 # steps to create a Krane instance using docker
 create_krane_docker(){
-  echo "Stopping Krane"
+  echo "Stopping Krane (if exists)"
   docker stop krane
 
-  echo -e "\nRemoving Krane instance"
+  echo -e "\nRemoving Krane instance (if exists)"
   docker rm krane
 
   echo -e "\nRemoving existing image"
@@ -37,10 +35,10 @@ create_krane_docker(){
   echo -e "\nPulling latest image"
   docker pull biensupernice/krane:latest -q
 
-  echo -e "\nStarting new instance"
+  echo -e "\nStarting new Krane instance"
   docker run -d --name=krane \
     -e LOG_LEVEL=info \
-    -e KRANE_PRIVATE_KEY="$KRANE_PRIVATE_KEY" \
+    -e KRANE_PRIVATE_KEY="${KRANE_PRIVATE_KEY:-uuidgen}" \
     -e DB_PATH="${DB_DIR/krane.db:-/tmp/krane.db}" \
     -e DOCKER_BASIC_AUTH_USERNAME="$DOCKER_BASIC_AUTH_USERNAME" \
     -e DOCKER_BASIC_AUTH_PASSWORD="$DOCKER_BASIC_AUTH_PASSWORD" \
@@ -53,18 +51,22 @@ create_krane_docker(){
     -v "${DB_DIR:-/tmp}":/tmp \
     -p 8500:8500 biensupernice/krane
 
-  echo -e "\nCleaning up older images"
+  echo -e "\nCleaning up dangling images"
   docker image prune -a -f
+
+  echo -e "\nBootstrap complete."
+  echo "Now you can try running 'docker ps' to view the running containers."
+  echo "For documentation on accessing this Krane instance visit https://www.krane.sh/#/docs/cli."
 }
 
-ensure_env DOCKER_BASIC_AUTH_USERNAME "Container registry username: (optional, will operate as an anonymous user. Note: May affect registry rate-limits)"
-ensure_secure_env DOCKER_BASIC_AUTH_PASSWORD "Container registry password: (optional, only if container registry username is being used)"
-ensure_env SSH_KEYS_DIR "Ssh keys directory: (optional, default /root/.ssh)"
-ensure_env DB_DIR "Krane database directory: (optional, default /tmp)"
-ensure_secure_env KRANE_PRIVATE_KEY "Krane private key: (required, for signing client requests)"
+ensure_env KRANE_PRIVATE_KEY "Krane private key (optional, used for signing client requests. default uuid)"
+ensure_env DOCKER_BASIC_AUTH_USERNAME "Container registry username (optional, will operate as an anonymous user)"
+ensure_secure_env DOCKER_BASIC_AUTH_PASSWORD "Container registry password (optional, will operate as an anonymous user)"
+ensure_env SSH_KEYS_DIR "SSH keys directory (optional, default /root/.ssh)"
+ensure_env DB_DIR "Krane database directory (optional, default /tmp)"
 ensure_env PROXY_ENABLED "Network proxy enabled? (default true, Note: if aliases are needed set as true)"
-ensure_env PROXY_DASHBOARD_ALIAS "Network proxy dashboard alias: (alias for the proxy dashboard, ie. monitor.example.com)"
-ensure_env PROXY_DASHBOARD_SECURE "Network proxy secure? (default true, enable https on the network proxy)"
-ensure_env LETSENCRYPT_EMAIL "Certificate email: (optional, email used to generate https/tls certificates. Note: Must be valid for let's encrypt to generate certs)"
+ensure_env PROXY_DASHBOARD_ALIAS "Network proxy dashboard alias (domain alias for the proxy dashboard, for example monitor.example.com)"
+ensure_env PROXY_DASHBOARD_SECURE "Network proxy secure? (default true, enables https on the network proxy. Note: should be set to false for 'localhost')"
+ensure_env LETSENCRYPT_EMAIL "Certificate email: (optional, email used to generate https/tls certificates. Note: Must be a valid email for Let's Encrypt to properly generate certs. Ignore for 'localhost')"
 
 create_krane_docker
