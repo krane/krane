1fc023766ddf257a4873f116813d803350356ef5
David Castaneda <dcast188@fiu.edu>
2021-01-23 19:27:08 -0500

    (feat) auth tests

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -2,26 +2,44 @@ package auth
 
 import (
 	"errors"
+	"fmt"
+
+	"github.com/docker/distribution/uuid"
 
 	"github.com/krane/krane/internal/constants"
-	"github.com/krane/krane/internal/logger"
 	"github.com/krane/krane/internal/store"
 )
 
-func GetClientServerPhrase(requestID string) (string, error) {
+// GetAuthenticationPhrase returns the generate phrase for a given request id
+func GetAuthenticationPhrase(requestID string) (string, error) {
 	bytes, err := store.Client().Get(constants.AuthenticationCollectionName, requestID)
 	if err != nil {
 		return "", err
 	}
 
 	if bytes == nil || len(bytes) == 0 {
-		logger.Warn("Invalid request id")
-		return "", errors.New("unable to authenticate, invalid request id")
+		return "", errors.New("invalid request id")
 	}
 
 	return string(bytes), nil
 }
 
-func RevokeClientRequestID(requestID string) error {
+// CreateAuthenticationPhrase returns a request id (uuid) and a phrase used by the client for authentication
+func CreateAuthenticationPhrase() (string, string, error) {
+	reqID := uuid.Generate().String()
+	phrase := []byte(fmt.Sprintf("Krane authentication request id: %s", reqID))
+
+	if err := store.Client().Put(constants.AuthenticationCollectionName, reqID, phrase); err != nil {
+		if err := store.Client().Remove(constants.AuthenticationCollectionName, reqID); err != nil {
+			return "", "", err
+		}
+		return "", "", err
+	}
+
+	return reqID, string(phrase), nil
+}
+
+// RevokeAuthenticationRequest removes the request from the authentication collection
+func RevokeAuthenticationRequest(requestID string) error {
 	return store.Client().Remove(constants.AuthenticationCollectionName, requestID)
 }
---
+++ internal/auth/auth_test.go
@@ -0,0 +1,53 @@
+package auth
+
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/krane/krane/internal/test"
+)
+
+func TestMain(m *testing.M) {
+	test.SetupDb()
+
+	code := m.Run()
+
+	test.TeardownDb()
+	os.Exit(code)
+}
+
+func TestGetAuthenticationPhrase(t *testing.T) {
+	reqID, phrase, err := CreateAuthenticationPhrase()
+	assert.Nil(t, err)
+
+	phrase2, err := GetAuthenticationPhrase(reqID)
+	assert.Nil(t, err)
+	assert.Equal(t, phrase, phrase2)
+}
+
+func TestGetAuthenticationPhraseReturnsErrWhenMissingRequestID(t *testing.T) {
+	phrase, err := GetAuthenticationPhrase("0")
+	assert.Error(t, err, "invalid request id")
+	assert.Empty(t, phrase)
+}
+
+func TestCreateAuthenticationPhrase(t *testing.T) {
+	reqID, phrase, err := CreateAuthenticationPhrase()
+	assert.Nil(t, err)
+	assert.NotEmpty(t, reqID)
+	assert.NotEmpty(t, phrase)
+}
+
+func TestRevokeAuthenticationRequest(t *testing.T) {
+	reqID, _, err := CreateAuthenticationPhrase()
+	assert.Nil(t, err)
+
+	err = RevokeAuthenticationRequest(reqID)
+	assert.Nil(t, err)
+
+	phrase, err := GetAuthenticationPhrase(reqID)
+	assert.Error(t, err, "invalid request id")
+	assert.Empty(t, phrase)
+}
