a2c2ef383fbc5d4a6538417ba4f183036269b499
david-castaneda <david9castaneda@gmail.com>
2021-06-27 23:38:40 -0400

    feat: Acquire a lock when emitting deployment events

--- internal/deployment/events.go
+++ internal/deployment/events.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"encoding/json"
 	"io"
+	"sync"
 
 	"github.com/gorilla/websocket"
 
@@ -13,17 +14,16 @@ import (
 type EventEmitter struct {
 	Deployment string
 	JobID      string
-	Phase      Phase
 	Clients    []*websocket.Conn
 }
 
 type Event struct {
 	JobID   string `json:"job_id"`
 	Message string `json:"message"`
-	Phase   Phase  `json:"phase"`
 }
 
 var eventClients = make(map[string][]*websocket.Conn)
+var eventsMutex = &sync.Mutex{}
 
 func createEventEmitter(deployment string, jobID string) *EventEmitter {
 	return &EventEmitter{
@@ -37,13 +37,14 @@ func createEventEmitter(deployment string, jobID string) *EventEmitter {
 // In order to allow clients to filter events for specific deployment runs, the job id
 // was added into the event payload, the job id is returned when triggering a deployment run.
 func (e EventEmitter) emit(message string) {
-	go func(clients []*websocket.Conn, jobID string, deployment string, phase Phase) {
+	go func(clients []*websocket.Conn, jobID string, deployment string) {
 		for _, client := range clients {
 			bytes, _ := json.Marshal(Event{
 				JobID:   jobID,
 				Message: message,
-				Phase:   phase,
 			})
+
+			eventsMutex.Lock()
 			if err := client.WriteMessage(websocket.TextMessage, bytes); err != nil {
 				// this will log when a client has disconnected at which point the
 				// connection is not valid causing a write error. This should not
@@ -52,8 +53,9 @@ func (e EventEmitter) emit(message string) {
 				UnSubscribeFromDeploymentEvents(client, deployment)
 				return
 			}
+			eventsMutex.Unlock()
 		}
-	}(e.Clients, e.JobID, e.Deployment, e.Phase)
+	}(e.Clients, e.JobID, e.Deployment)
 }
 
 // emitStream broadcast a stream of data to all clients connected to the deployment.
@@ -69,9 +71,9 @@ func (e EventEmitter) emitStream(reader io.Reader) {
 		data, _ := json.Marshal(Event{
 			JobID:   e.JobID,
 			Message: string(bytes),
-			Phase:   e.Phase,
 		})
 		for _, client := range e.Clients {
+			eventsMutex.Lock()
 			if err := client.WriteMessage(websocket.TextMessage, data); err != nil {
 				// this will log when a client has disconnected at which point the
 				// connection is not valid causing a write error. This should not
@@ -80,7 +82,26 @@ func (e EventEmitter) emitStream(reader io.Reader) {
 				UnSubscribeFromDeploymentEvents(client, e.Deployment)
 				return
 			}
+			eventsMutex.Unlock()
 		}
+	}
+}
+
+func (e EventEmitter) closeStream(message string) {
+	for _, client := range e.Clients {
+		data, _ := json.Marshal(Event{
+			JobID:   e.JobID,
+			Message: message,
+		})
+		eventsMutex.Lock()
+		if err := client.WriteMessage(websocket.TextMessage, data); err != nil {
+			// this will log when a client has disconnected at which point the
+			// connection is not valid causing a write error. This should not
+			// affect other clients or streaming logs in general.
+			logger.Debugf("client %v disconnected", client.RemoteAddr())
+		}
+		UnSubscribeFromDeploymentEvents(client, e.Deployment)
+		eventsMutex.Unlock()
 	}
 }
 
